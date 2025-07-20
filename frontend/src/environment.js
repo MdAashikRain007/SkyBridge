85affedf14bc48587686915607da8beb6450a993
MdAashikRain007 <aashikmd2256@gmail.com>
2025-07-20 14:36:00 +0530

    add backend url

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,5 @@
 let IS_PROD = true;
-const server = IS_PROD ?
-      "http://localhost:8080" :
-      
-     "https://apnacollegebackend.onrender.com" 
+const server = IS_PROD ? "https://skybridge-h4w0.onrender.com" :  "http://localhost:8080" 
 
 
 export default server;
